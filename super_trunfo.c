55b36119d0a30d3653d3102734b3e0f6d2540017
Kristiano244 <cristiano.wagner12@gmail.com>
2025-08-24 15:19:31 -0300

    super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -4,7 +4,12 @@
 int main(){
     /*Irei fazer uma pequena explicacao para o usuario,
     para que ele saiba usar o sistema.*/
-    printf("Bem-vindo ao Super Trunfo em C Versao Cidades.\nNeste jogo, voce sera o criador das cartas. Para comecar, insira os dados de duas cidades \nconforme seja solicitado.\nEssas informacoes vao completar as cartas e permitir que você jogue comparando \nqual cidade vence em cada categoria.\nPrepare-se para descobrir qual delas sera a campea!\n");
+    printf("Bem-vindo ao Super Trunfo em C Versao Cidades.\n");
+    printf("Neste jogo, voce sera o criador das cartas. Para comecar, insira os dados de duas cidades \n");
+    printf("conforme seja solicitado.\n");
+    printf("Essas informacoes vao completar as cartas e permitir que você jogue comparando \n");
+    printf("qual cidade vence em cada categoria.\n\n");
+    printf("Prepare-se para descobrir qual delas sera a campea!\n");
     printf("\n"); //Coloquei somente para deixar a interface mais limpa para o usuario.
     
     // Agora irei definir as variaveis.
@@ -16,11 +21,13 @@ int main(){
 
     /*Apos as variaveis estarem definidas, vou solicitar que o usuario
     de valores e nomes para a primeira carta.*/
-    printf("Carta 1\n");
+    printf("**********************\n");
+    printf("   DADOS - CARTA 1\n");
+    printf("**********************\n");
     printf("Insira uma letra de 'A' a 'H' (representando um dos oito estados): ");
     scanf(" %c", &estado1);
     printf("Insira a letra do estado seguida de um número de 01 a 04 (ex: A01, B03): ");
-    scanf(" %s", &codigo1);
+    scanf(" %s", codigo1);
     printf("Insira o nome da cidade: ");
     scanf(" %[^\n]", &cidade1);
     printf("Insira o numero de habitantes da cidade: ");
@@ -32,16 +39,21 @@ int main(){
     printf("Insira a quantidade de pontos turisticos na cidade: ");
     scanf("%d", &pontos_turisticos1);
 
-    /*Uma breve mensagem mostrando ao usuario que o preenchimento da primeira carta foi concluido,
-    e agora ele ira preencher os dados da segunda carta.*/
+    //Uma breve mensagem mostrando ao usuario que o preenchimento da primeira carta foi concluido com sucesso.
     printf("\n"); //Para ficar mais separado, e mais limpa a tela para o usuario.
-    printf("Carta 1 completa, agora preencha os dados da Carta 2.\n");
-    printf("\n");
-    printf("Carta 2\n");
+    printf("\n===============================================================\n");
+    printf("            CARTA 1 REGISTRADA COM SUCESSO!\n");
+    printf("         Agora vamos preencher os dados da CARTA 2.\n");
+    printf("===============================================================\n\n");
+    
+    //Agora o usuario vai comecar a preencher os dados da segunda carta.
+    printf("**********************\n");
+    printf("   DADOS - CARTA 2\n");
+    printf("**********************\n");
     printf("Insira uma letra de 'A' a 'H' (representando um dos oito estados): ");
     scanf(" %c", &estado2);
     printf("Insira a letra do estado seguida de um número de 01 a 04 (ex: A01, B03): ");
-    scanf(" %s", &codigo2);
+    scanf(" %s", codigo2);
     printf("Insira o nome da cidade: ");
     scanf(" %[^\n]", &cidade2);
     printf("Insira o numero de habitantes da cidade: ");
@@ -58,7 +70,9 @@ int main(){
     printf("\n");
     printf("Aqui estao os dados das cartas cadastradas.\n");
     printf("\n");
-    printf("Carta 1\n");
+    printf("**********************\n");
+    printf("      CARTA 1\n");
+    printf("**********************\n");
     printf("Estado: %c\n", estado1);
     printf("Codigo: %s\n", codigo1);
     printf("Nome da Cidade: %s\n", cidade1);
@@ -68,7 +82,9 @@ int main(){
     printf("Numero de Pontos Turisticos: %d\n", pontos_turisticos1);
 
     printf("\n"); //Somente para separarar os dados da primeira carta com os dados da segunda carta, de uma forma que nao fique muito junto.
-    printf("Carta 2\n");
+    printf("**********************\n");
+    printf("      CARTA 2\n");
+    printf("**********************\n");
     printf("Estado: %c\n", estado2);
     printf("Codigo: %s\n", codigo2);
     printf("Nome da Cidade: %s\n", cidade2);
